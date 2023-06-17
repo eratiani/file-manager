16ba50583cdc8dfb5cd2a7af24c2e57edcbf4c70
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-17 14:25:14 +0400

    add username loging

--- fileManager.js
+++ fileManager.js
@@ -1,8 +1,11 @@
 import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+import { stdin as input, stdout as output , argv as inputArgs } from 'node:process';
 
 const rl = readline.createInterface({ input, output });
 
+const args = inputArgs.slice(2)
+const userNmae = args[0].split('=')[1]
+console.log(`Welcome to the File Manager, ${userNmae}!`);
 const answer = await rl.question('What do you think of Node.js? ');
 
 console.log(`Thank you for your valuable feedback: ${answer}`);
